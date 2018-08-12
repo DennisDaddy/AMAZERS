482581e2bd76bc50ad1e0ec16ae2ad93c5712c38
DennisDaddy <softteez95@gmail.com>
2018-08-12 10:08:48 +0300

    Add before_filter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
   validates :user_id, presence: true
