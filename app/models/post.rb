bb25e293de6c401408252ee44a4fe86e175a54d5
DennisDaddy <softteez95@gmail.com>
2018-08-12 09:07:29 +0300

    Add associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :content, presence: true
+
 end
