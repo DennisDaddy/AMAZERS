0c0c578e99b072c14ffb5075eaba917ae8410f11
DennisDaddy <softteez95@gmail.com>
2018-08-12 16:29:15 +0300

    Add migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
+  belongs_to :category
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true
